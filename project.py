11245b82152d4c271691a38657d83a283898edde
Unknown <leander.vandenheuvel@gmail.com>
2017-12-21 14:05:52 +0100

    TensorFlow addition, not working yet

--- project.py
+++ project.py
@@ -30,7 +30,10 @@ with open('semeion.data.txt') as inputfile:
 #[float(i) for i in data]
 x=16
 y=16
-plt.imshow(np.reshape(data[170],(x,y)))
+data_x = np.asarray(data)
+data_y = np.asarray(data_labels)
+
+#plt.imshow(np.reshape(data[170],(x,y)))
 def convert_labels(data_labels):
     text_labels = []
     for digit in data_labels:
@@ -40,8 +43,33 @@ def convert_labels(data_labels):
                 text_labels.append(i)
             i+=1
     return text_labels
-text_labels = convert_labels(data_labels)
+#text_labels = convert_labels(data_labels)
 
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+#from tensorflow.examples.tutorials.mnist import input_data
+#mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
+x = tf.placeholder(tf.float32, [None, 256])
+
+W = tf.Variable(tf.zeros([1593, 256]))
+b = tf.Variable(tf.zeros([10]))
+
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+sess = tf.InteractiveSession()
+
+tf.global_variables_initializer().run()
+print('training model...')
+for _ in range(1000):
+  #batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(train_step, feed_dict={x: data_x, y_: data_y})
+  
+#correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+#accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+#print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
