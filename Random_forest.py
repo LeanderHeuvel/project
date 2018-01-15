05227e0fdc08f4de6c1f95edee699aaba2262679
Erven, L.W.A.T. (Loes) <l.erven@student.ru.nl>
2018-01-15 10:47:48 +0100

    plotten van tree depths toegevoegd

--- Random_forest.py
+++ Random_forest.py
@@ -8,25 +8,28 @@ Created on Mon Jan 15 09:07:06 2018
 
 import project as pj
 from sklearn.ensemble import RandomForestClassifier
-
+from matplotlib import pyplot as plt
 
 data_y_train_ints = pj.convert_labels(pj.data_y_train)
 data_y_test_ints = pj.convert_labels(pj.data_y_test)
 
-clf = RandomForestClassifier(max_depth=2, random_state=0)
-clf.fit(pj.data_x_train, data_y_train_ints)
-RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini', 
-                       max_depth=2, max_features='auto', max_leaf_nodes=None, 
-                       min_impurity_decrease=0.0, min_impurity_split=None, 
-                       min_samples_leaf=1, min_samples_split=2, 
-                       min_weight_fraction_leaf=0.0, n_estimators=10, n_jobs=1, 
-                       oob_score=False, random_state=0, verbose=0, warm_start=False)
-feature_importances = clf.feature_importances_
-predicted_ys = clf.predict(pj.data_x_test)
-print (predicted_ys)
+accuracies = []
+depths = []
+for i in range(1,200,5):
+    print(i)
+    clf = RandomForestClassifier(max_depth=None, n_estimators=i, max_features=10) 
+    clf.fit(pj.data_x_train, data_y_train_ints)
 
-i = 0
-for x in range(0,1393):
-    if predicted_ys[x] == data_y_test_ints[x]:
-        i+=1
-print (i/1393)
+    feature_importances = clf.feature_importances_
+    predicted_ys = clf.predict(pj.data_x_test)
+    #print (predicted_ys)
+
+    j = 0
+    for x in range(0,1393):
+        if predicted_ys[x] == data_y_test_ints[x]:
+            j+=1
+    print (j/1393)
+    accuracies.append(j/1393)
+    depths.append(i)
+plt.plot(depths,accuracies)
+    
