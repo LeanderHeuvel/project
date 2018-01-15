883b4453018a23777a6538ffa666640d20d23f77
Erven, L.W.A.T. (Loes) <l.erven@student.ru.nl>
2018-01-15 09:59:13 +0100

    accuracy tellen toegevoegd

--- Random_forest.py
+++ Random_forest.py
@@ -15,7 +15,12 @@ data_y_test_ints = pj.convert_labels(pj.data_y_test)
 
 clf = RandomForestClassifier(max_depth=2, random_state=0)
 clf.fit(pj.data_x_train, data_y_train_ints)
-RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini', max_depth=2, max_features='auto', max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None, min_samples_leaf=1, min_samples_split=2, min_weight_fraction_leaf=0.0, n_estimators=10, n_jobs=1, oob_score=False, random_state=0, verbose=0, warm_start=False)
+RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini', 
+                       max_depth=2, max_features='auto', max_leaf_nodes=None, 
+                       min_impurity_decrease=0.0, min_impurity_split=None, 
+                       min_samples_leaf=1, min_samples_split=2, 
+                       min_weight_fraction_leaf=0.0, n_estimators=10, n_jobs=1, 
+                       oob_score=False, random_state=0, verbose=0, warm_start=False)
 feature_importances = clf.feature_importances_
 predicted_ys = clf.predict(pj.data_x_test)
 print (predicted_ys)
