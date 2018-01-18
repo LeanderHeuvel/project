0cef2b17d15c1ebfa0debd115df02ce8b3f9aab9
Unknown <leander.vandenheuvel@gmail.com>
2018-01-18 11:40:00 +0100

    Auto stash before merge of "master" and "origin/master"

--- t_flow.py
+++ t_flow.py
@@ -9,9 +9,10 @@ import tensorflow as tf
 import Semeion_data_loader as data
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+#from tensorflow.examples.tutorials.mnist import input_data
+#mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 sess = tf.InteractiveSession()
 
@@ -55,10 +56,10 @@ b_conv2 = bias_variable([64])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
+W_fc1 = weight_variable([4 * 4 * 64, 1024])
 b_fc1 = bias_variable([1024])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 4*4*64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 
@@ -76,19 +77,21 @@ correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print('training model...')
-
+accuracies=[]
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
-  for i in range(20000):
+  for i in range(5000):
     batch_xs, batch_ys = next_batch(50)
     #batch = mnist.train.next_batch(50)
     if i % 100 == 0:
-      train_accuracy = accuracy.eval(feed_dict={
+        train_accuracy = accuracy.eval(feed_dict={
               #x: batch[0], y_: batch[1], keep_prob: 1.0})
               x: batch_xs, y_: batch_ys, keep_prob: 1.0})
-      print('step %d, training accuracy %g' % (i, train_accuracy))
+        accuracies.append(accuracy.eval(feed_dict={
+      x: data.x_test, y_: data.y_test, keep_prob: 1.0}))
+        print('step %d, training accuracy %f' % (i, train_accuracy))
     train_step.run(feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
     #train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
   print('test accuracy %g' % accuracy.eval(feed_dict={
       x: data.x_test, y_: data.y_test, keep_prob: 1.0}))
-#print('test accuracy %g' % accuracy.eval(feed_dict={ x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+  plt.plot(accuracies)
