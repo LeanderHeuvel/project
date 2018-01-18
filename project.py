eccfe1095ee35ef3593b5b6b3f70f46b537fdd02
Unknown <leander.vandenheuvel@gmail.com>
2018-01-18 10:34:08 +0100

    addition of t_flow.py

--- project.py
+++ project.py
@@ -12,16 +12,18 @@ import Semeion_data_loader as data
 #from tensorflow.examples.tutorials.mnist import input_data
 #mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 #mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+x = tf.placeholder(tf.float32, shape=[None, 256])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+ 
 x = tf.placeholder(tf.float32, [None, 256])
     
 W = tf.Variable(tf.zeros([256, 10]))
 b = tf.Variable(tf.zeros([10]))
 
-y = tf.nn.softmax(tf.matmul(x, W) + b)
+y = tf.matmul(x,W) + b
 
-y_ = tf.placeholder(tf.float32, [None, 10])
-
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
@@ -42,7 +44,7 @@ def next_batch(size):
 
 for _ in range(1500):
     batch_xs, batch_ys = next_batch(100)
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    train_step.run(feed_dict={x: batch_xs, y_: batch_ys})
   
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
