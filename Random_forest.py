c930558e7e386e48d9a3ce0a41b02e454a6fbbf2
Erven, L.W.A.T. (Loes) <l.erven@student.ru.nl>
2018-01-18 11:49:33 +0100

    averages loop

--- Random_forest.py
+++ Random_forest.py
@@ -11,12 +11,8 @@ import matplotlib.pyplot as plt
 import Semeion_data_loader as data
 
 
-
-clf = RandomForestClassifier(max_depth=40, n_jobs=-1, random_state=0, n_estimators=250)
-clf.fit(data.x_train, data.y_train_ints)
-
-NR_ITERS = 2 #one iteration is run entirely over the train and test sets
-depths = []
+NR_ITERS = 20 #one iteration is run entirely over the train and test sets
+estimators = []
 all_accs = [] # list of all accuracies over all iterations to calculate averages
 for _ in range (0,NR_ITERS):
     accuracies = [] # list of the accuracies of one iteration
@@ -24,21 +20,22 @@ for _ in range (0,NR_ITERS):
         #print(i)
         clf = RandomForestClassifier(max_depth=None, n_estimators=i, max_features=10, n_jobs=-1) 
         clf.fit(data.x_train, data.y_train_ints)
-    
+        
         predicted_ys = clf.predict(data.x_test)
         #print (predicted_ys)
-    
+
         j = 0 #to calculate accuracy
         for x in range(0,1393):
             if predicted_ys[x] == data.y_test_ints[x]:
                 j+=1
-            #print (j/1393)
+                #print (j/1393)
         accuracies.append(j/1393)
-        if len(depths) < 40:
-            depths.append(i)
-       # feature_importances = clf.feature_importances_
-        #plt.imshow(np.reshape(feature_importances,(16,16)))
-        #plt.show()
+        if len(estimators) < 40:
+            estimators.append(i)
+   # feature_importances = clf.feature_importances_
+    #plt.imshow(np.reshape(feature_importances,(16,16)))
+    #plt.show()
+
     all_accs.append(accuracies)
 all_accs = np.array(all_accs)
 all_accs = all_accs.T
@@ -51,5 +48,9 @@ for i in range(0,40):
     avg_accs = avg_accs.T
     avg_accs = avg_accs.tolist()
 
-plt.plot(depths, avg_accs)
+plt.plot(estimators, avg_accs)
+plt.x_label("nr. of trees")
 #todo, plot fatsoenlijk mooi maken
+
+
+plt.plot(estimators, accuracies)
