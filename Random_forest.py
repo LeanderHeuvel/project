d208aa8323f9cecda41dcca06fbacebae7cd4985
Ruaiks <loes.erven@gmail.com>
2018-01-28 23:10:30 +0100

    code updated, fixed

--- Random_forest.py
+++ Random_forest.py
@@ -3,7 +3,7 @@
 """
 Created on Mon Jan 15 09:07:06 2018
 
-@author: leanderheuvel
+@author: loeserven
 """
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
@@ -14,7 +14,7 @@ import Semeion_data_loader as data
 NR_ITERS = 20 #one iteration is run entirely over the train and test sets
 estimators = []
 all_accs = [] # list of all accuracies over all iterations to calculate averages
-for k in range (0,NR_ITERS):
+for iteration in range (0,NR_ITERS):
     accuracies = [] # list of the accuracies of one iteration
     for i in range(1,200,5):
         #print(i)
@@ -24,7 +24,7 @@ for k in range (0,NR_ITERS):
         predicted_ys = clf.predict(data.x_test)
         #print (predicted_ys)
 
-        j = 0 #to calculate accuracy
+        j = 0.0 #to calculate accuracy
         for x in range(0,1393):
             if predicted_ys[x] == data.y_test_ints[x]:
                 j+=1
@@ -32,7 +32,7 @@ for k in range (0,NR_ITERS):
         accuracies.append(j/1393)
         if len(estimators) < 40:
             estimators.append(i)
-    print(k)
+    print(iteration)
    # feature_importances = clf.feature_importances_
     #plt.imshow(np.reshape(feature_importances,(16,16)))
     #plt.show()
@@ -45,9 +45,8 @@ all_accs = all_accs.tolist()
 avg_accs = []
 for i in range(0,40):
     avg_accs.append(sum(all_accs[i])/NR_ITERS)
-    avg_accs = np.array(avg_accs)
-    avg_accs = avg_accs.T
-    avg_accs = avg_accs.tolist()
+avg_accs = np.array(avg_accs)
+avg_accs = avg_accs.T
+avg_accs = avg_accs.tolist()
 
 plt.plot(estimators, avg_accs)
-#todo, plot fatsoenlijk mooi maken
